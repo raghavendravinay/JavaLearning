735035cfd0dd8f3d059fd2b32b1b525938297511
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-04-24 22:55:03 +0530

    Modified the Fibonacci program

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,19 +4,19 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter a value");
-		int num=sc.nextInt();
-		int first=0; 
-		int second=1;
-		int third;
-		System.out.println(first);
-		System.out.println(second);
+		System.out.println("Enter a Number");
+		Long num=sc.nextLong();
+		Long first=0L;
+		Long second=1L;
+		Long third;
+		System.out.print(first+" ");
+		System.out.print(second+" ");
+		
 		while(num-->0) {
 			third=first+second;
-			System.out.println(third);
+			System.out.print(third+" ");
 			first=second;
 			second=third;
 		}
-		
 	}
 }
