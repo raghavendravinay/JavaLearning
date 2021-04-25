95eda2fde365df7ff08d7239dbc8e92b7d957c63
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-04-25 16:13:28 +0530

    Added Prime number program

---
+++ src/prime_number/PrimeNumber1.java
@@ -0,0 +1,18 @@
+package prime_number;
+
+import java.util.Scanner;
+
+public class PrimeNumber1 {
+
+	public static void main(String []args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a number");   // Prime Numbers: 1, 2, 3, 5, 7, 11, 13, 17, 19, 23....
+		int num=sc.nextInt();
+		for(int i=2; i<num; i++) {
+			if(num%i==0) {
+				System.out.println(num+" is not a prime");
+			}
+		}
+		
+	}
+}
