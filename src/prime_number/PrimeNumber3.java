dfb4f987fb466ac32779154deabae83583076377
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-04-25 19:34:33 +0530

    Added Prime number. checked upto num/2

---
+++ src/prime_number/PrimeNumber3.java
@@ -0,0 +1,25 @@
+package prime_number;
+
+import java.util.Scanner;
+
+public class PrimeNumber3 {
+
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=sc.nextInt();  // Prime Numbers: 1, 2, 3, 5, 7, 11, 13, 17, 19, 23....
+		boolean isPrime=false;
+		for(int i=2; i<=num/2;i++) {
+			if(num%i==0) {
+				isPrime=true;
+				break;
+			}
+		}
+		if(isPrime) {
+			System.out.println(num+"  is not a prime number"); //Prints given number is prime or not
+		}
+		else {
+			System.out.println(num+" is a prime number");
+		}
+	}
+}
