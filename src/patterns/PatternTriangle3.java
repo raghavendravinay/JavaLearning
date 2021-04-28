09e23ac671008717150ee7781ecc09581d69e169
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-04-28 08:38:02 +0530

    Added triangle pattern program

---
+++ src/patterns/PatternTriangle3.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class PatternTriangle3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=sc.nextInt();
+		for(int i=num; i>0; i--) {
+			for(int j=i-1; j>0; j--) {
+				System.out.print(" ");
+			}
+			for(int k=i; k<=num; k++){
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+}
