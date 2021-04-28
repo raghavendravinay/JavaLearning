59d28151a68176f955210fbe822ada36eab7b3be
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-04-28 08:01:52 +0530

    Added Boarder Pattern program

---
+++ src/patterns/BoarderPattern.java
@@ -0,0 +1,23 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class BoarderPattern {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=sc.nextInt();
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<=num; j++) {
+				if(i==1||i==num||j==1||j==num) {
+					System.out.print("* ");
+				}
+				else {
+				System.out.print("  ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
