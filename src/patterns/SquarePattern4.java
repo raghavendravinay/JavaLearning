0027d91c342ae90a714043f4c3ebdc052f2d4b7a
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-04-28 09:40:11 +0530

    Added square pattern

---
+++ src/patterns/SquarePattern4.java
@@ -0,0 +1,18 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class SquarePattern4 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=sc.nextInt();
+		for(int i=num; i>0; i--) {
+			for(int j=1; j<=num; j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println("");
+		}
+	}
+}
