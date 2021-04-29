0fdd331055128782a71df069dd38ffb534a10fb0
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-04-29 09:08:42 +0530

    Added triangle pattern

---
+++ src/patterns/PatternTriangle12.java
@@ -0,0 +1,22 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class PatternTriangle12 {
+
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number");
+		int num=sc.nextInt();
+		for(int i=1; i<=num;i++) {
+			for(int j=1;j<=num-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i; k++) {
+				System.out.print(k);
+			}
+			System.out.println("");
+		}
+	}
+
+}
