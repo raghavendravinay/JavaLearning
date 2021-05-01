adf559809c6e27052e4a9d98cc5cccef65276c6c
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-05-01 12:59:44 +0530

    Added new pyramid pattern

---
+++ src/patterns/PyramidPattern7.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class PyramidPattern7 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=sc.nextInt();
+		for(int i=num; i>0; i--) {
+			for(int j=1; j<=num-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<2*i; k++) {
+				System.out.print("*");
+			}
+		System.out.println("");	
+		}
+	}
+}
