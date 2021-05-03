9e1fc25ded909f092ebc3c982a823465b28560a8
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-05-03 09:45:22 +0530

    modified the code

--- src/patterns/SquarePattern6.java
+++ src/patterns/SquarePattern6.java
@@ -8,7 +8,7 @@ public class SquarePattern6 {
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number"); //Enter odd number
+		System.out.println("Enter any odd number"); 
 		int num=sc.nextInt();
 		for(int i=1; i<=num; i++)
 		{
