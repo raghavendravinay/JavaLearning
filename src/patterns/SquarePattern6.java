68c1aad8a91f6b00e773c76734c5ca9fe9e74d0f
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-05-03 09:40:01 +0530

    Added new square Pattern

---
+++ src/patterns/SquarePattern6.java
@@ -0,0 +1,28 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class SquarePattern6 {
+
+	//Prints 'O' at center of the square and 'X' at remaining places
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number"); //Enter odd number
+		int num=sc.nextInt();
+		for(int i=1; i<=num; i++)
+		{
+			for(int j=1; j<=num; j++) 
+			{
+				if(i==(num+1)/2 && j==(num+1)/2)
+				{
+					System.out.print("O ");
+				}
+				else {
+					System.out.print("X ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
