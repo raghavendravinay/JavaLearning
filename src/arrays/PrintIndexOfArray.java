ede77163725b94dbfcab6e1f862a3a0080f1c4c1
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-05-07 23:59:59 +0530

    Added a code to print indexes of a given number present in the array

---
+++ src/arrays/PrintIndexOfArray.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class PrintIndexOfArray {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int k=0,l=0;
+		System.out.println("Enter size of the array:");
+		int n=sc.nextInt();
+		System.out.println("Enter values into array");
+		int arr1[]=new int[n];
+		int arr2[]=new int[n];
+		for(int i=0; i<n;i++) {
+			arr1[i]=sc.nextInt();
+		}
+		for(int j=0; j<n; j++) {
+			if(arr1[j]==5) {
+				arr2[k]=j;
+				l=k;
+				k++;
+			}
+		}
+		System.out.println(k+"index(es) ");
+		for(int i=0; i<=l; i++) {
+			if(k>0) {
+				System.out.print(arr2[i]+" ");
+			}
+			else {
+				System.out.println("-1");
+			}
+		}
+		
+	}
+}
