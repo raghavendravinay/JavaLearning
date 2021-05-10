97fa22e32f5f6dcd33f85d71ca805ceb50b967c1
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-05-10 09:03:36 +0530

    Added code to print largest number from the given array using 2nd approach

---
+++ src/arrays/MaxValueOfArray2.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class MaxValueOfArray2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter array size");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("enter values into array");
+		for(int i=0; i<n; i++){
+			arr[i]=sc.nextInt();
+		}
+		int max=Integer.MIN_VALUE;
+		for(int i=0; i<n-1; i++)
+		{
+		if(max<arr[i+1]) {
+			max=arr[i+1];
+		}
+		}
+		System.out.println(max);
+		}
+}
