c94c0ef3b976f256d5de54c13de7876afe51bb4e
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-05-10 08:57:35 +0530

    added a code to print the largest number from the given numbers until -1(exclude -1) using for loop 3rd approach

---
+++ src/arrays/FindLargeNumbersFromGivenNumbers7.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class FindLargeNumbersFromGivenNumbers7 {
+
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter values");
+		int num=sc.nextInt();
+		int max=Integer.MIN_VALUE;
+		for(;num!=-1;) {
+			
+			if(max<num && num!=-1)
+				max=num;
+			num=sc.nextInt();
+			System.out.println(max);
+		}
+		
+	}
+}
+
