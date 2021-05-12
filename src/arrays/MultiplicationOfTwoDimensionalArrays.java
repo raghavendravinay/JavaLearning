ef50a88d0500303119b84c647557c50366608ba1
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-05-12 07:56:46 +0530

    modified the matrix multiplication code

--- src/arrays/MultiplicationOfTwoDimensionalArrays.java
+++ src/arrays/MultiplicationOfTwoDimensionalArrays.java
@@ -9,34 +9,46 @@ import java.util.Scanner;
 public class MultiplicationOfTwoDimensionalArrays {
 
 	public static void main(String[] args) {
-		int i, j;
+		int i, j, k;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter array size");
-		int size=sc.nextInt();
+		System.out.println("Enter row size and column size for 1st matrix");
+		int r1=sc.nextInt();
+		int c1=sc.nextInt();
+		System.out.println("Enter row size and column size for 2nd matrix");
+		int r2=sc.nextInt();
+		int c2=sc.nextInt();
 		System.out.println("Enter values into 1st array");
-		int[][] array1=new int[size][size];
-		int[][] array2=new int[size][size];
-		int[][] array3=new int[size][size];
-		for(i=0; i<array1.length; i++) {
-			for(j=0; j<array1.length; j++) {
-				array1[i][j]=sc.nextInt();
+		int[][] A=new int[r1][c1];
+		int[][] B=new int[r2][c2];
+		int[][] AB=new int[r1][c2];
+		for(i=0; i<r1; i++) {
+			for(j=0; j<c1; j++) {
+				A[i][j]=sc.nextInt();
 			}
 		}
 		System.out.println("Enter values into 2nd array");
-		for(i=0; i<array2.length; i++) {
-			for(j=0; j<array2.length; j++) {
-				array2[i][j]=sc.nextInt();
+		for(i=0; i<r2; i++) {
+			for(j=0; j<c2; j++) {
+				B[i][j]=sc.nextInt();
 			}
 		}
-
-		for(i=0; i<array3.length; i++) {
-			for(j=0; j<array3.length; j++) {
-				array3[i][j]=array1[i][j]*array2[i][j];
-//				System.out.print(array3[i][j]+" ");
-				System.out.println("array1["+i+"]["+j+"] * "+"array2["+i+"]["+j+"]= "+array3[i][j] );
+		if(c1==r2) {
+			for(i=0; i<c2; i++) {
+				for(j=0; j<r1; j++) {
+					for(k=0; k<c2; k++) {
+						AB[i][j]=AB[i][j]+(A[i][k]*B[k][j]);
+					}
+				}
 			}
-//			System.out.println(" ");
+			for(i=0; i<r1; i++) {
+				for(j=0; j<c2; j++) {
+					System.out.print(AB[i][j]+" ");
+				}
+				System.out.println(" ");
+			}
 		}
-
+		else {
+			System.out.println("Matrix multiplication is not possible");
+		}
 	}
 }
