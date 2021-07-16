1c62daec9633b2645af5b9f023319349a4f41eb3
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-07-16 07:58:13 +0530

    Added changes

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 /**
  * This program is to find the given number is a perfect number or not.
+ * 
  * @author RaghavendraVinayM
  *
  */
@@ -9,25 +10,24 @@ import java.util.Scanner;
 public class PerfectNumber {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a number");	// Ex: num = 28
+		System.out.println("Enter a number"); // Ex: num = 28
 		int num = scanner.nextInt();
-		
-		if(num == isStaticNumber(num)) {
+
+		if (num == isStaticNumber(num)) {
 			System.out.println(num + " is a perfect number");
-		}
-		else {
+		} else {
 			System.out.println(num + " is not a perfect number");
 		}
 		scanner.close();
 	}
-	
+
 	public static int isStaticNumber(int num) {
-		
+
 		int add = 0;
-		for(int i = 1; i <= num / 2; i++) {
-			if(num % i == 0) {
+		for (int i = 1; i <= num / 2; i++) {
+			if (num % i == 0) {
 				add = add + i;
 			}
 		}
