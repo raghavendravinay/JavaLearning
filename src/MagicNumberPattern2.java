260ab532356c12156c1907c1569ec3164b355903
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-07-16 07:57:19 +0530

    Added changes

--- src/MagicNumberPattern2.java
+++ src/MagicNumberPattern2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 /**
  * This program is to find the given number is magic number or not.
+ * 
  * @author RaghavendraVinayM
  *
  */
@@ -9,26 +10,25 @@ import java.util.Scanner;
 public class MagicNumberPattern2 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a number"); 
-		int num = scanner.nextInt();  // ex: num = 325;
-		
-		if(isMagicNumber(num)) {
+		System.out.println("Enter a number");
+		int num = scanner.nextInt(); // ex: num = 325;
+
+		if (isMagicNumber(num)) {
 			System.out.println(num + " is a Magic Number");
-		}
-		else {
+		} else {
 			System.out.println(num + " is not a Magic Number");
 		}
-		
+
 		scanner.close();
 	}
-	
+
 	public static boolean isMagicNumber(int num) {
 		int rem, magic = 0;
-		
-		while(num > 9) {
-			while(num > 0) {
+
+		while (num > 9) {
+			while (num > 0) {
 				rem = num % 10;
 				magic = magic + rem;
 				num = num / 10;
@@ -36,11 +36,10 @@ public class MagicNumberPattern2 {
 			num = magic;
 			magic = 0;
 		}
-		
-		if(num == 1) {
+
+		if (num == 1) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
