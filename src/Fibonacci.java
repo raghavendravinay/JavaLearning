77812773c3c1dfb671acf064ba6c82b2189f2071
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-07-16 07:56:50 +0530

    Added changes

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 /**
  * This program is to print the Fibonacci series
+ * 
  * @author RaghavendraVinayM
  *
  */
@@ -9,29 +10,29 @@ import java.util.Scanner;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a Number");
-		long num=scanner.nextLong();
+		long num = scanner.nextLong();
 		long first = 0L;
 		long second = 1L;
-		
+
 		printFibonacci(first, second, num);
 		scanner.close();
 	}
-	
+
 	public static void printFibonacci(long first, long second, long num) {
-		
+
 		long third;
 		System.out.print(first + " ");
 		System.out.print(second + " ");
-		while(num-- > 0) {
+		while (num-- > 0) {
 			third = first + second;
 			System.out.print(third + " ");
 			first = second;
 			second = third;
 		}
-		
+
 	}
 
 }
