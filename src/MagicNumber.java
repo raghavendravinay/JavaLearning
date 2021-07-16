1b2c8d9441f3b3facc33781a9c0b88dbbcc77e5b
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-07-16 07:56:07 +0530

    added changes

--- src/MagicNumber.java
+++ src/MagicNumber.java
@@ -2,44 +2,43 @@ import java.util.Scanner;
 
 /**
  * This program is to find the given number is a magic number or not
+ * 
  * @author RaghavendraVinayM
  *
  */
 
 public class MagicNumber {
 
-	public static void main(String []args) {
-		
+	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a number");
 		int num = scanner.nextInt(); // Ex: num = 325;
-		
-		if(isMagicNumber(num)) {
+
+		if (isMagicNumber(num)) {
 			System.out.println(num + " is a Magic Number");
-		}
-		else {
+		} else {
 			System.out.println(num + " is not a Magic Number");
 		}
 		scanner.close();
 	}
-	
+
 	public static boolean isMagicNumber(int num) {
 		int rem, mag = 0;
-		
-		while(num > 0) {
+
+		while (num > 0) {
 			rem = num % 10;
 			mag = mag + rem;
 			num = num / 10;
-			if(num == 0 && mag > 9) {
+			if (num == 0 && mag > 9) {
 				num = mag;
 				mag = 0;
 			}
-			
+
 		}
-		if(mag == 1) {
+		if (mag == 1) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
