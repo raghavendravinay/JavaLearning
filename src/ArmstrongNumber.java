f5741030a78ae443f7800ff6a44a629e9a07e664
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-07-16 07:54:08 +0530

    modified the ArmStrongNumber

--- src/ArmstrongNumber.java
+++ src/ArmstrongNumber.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 /**
- * This program is to find the given number is Armstrong Number or not? 
+ * This program is to find the given number is Armstrong Number or not?
+ * 
  * @author RaghavendraVinayM
  *
  */
@@ -13,20 +14,18 @@ public class ArmstrongNumber {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a number");
 		int num = scanner.nextInt();
-		
-		if(isArmstrong(num)) {
+
+		if (isArmstrong(num)) {
 			System.out.println("Given value is an Armstrong number");
-		}
-		else {
+		} else {
 			System.out.println("Given value is not an Armstrong Number");
 		}
-		
+
 		int temp = armstrongNumber(num);
-		
-		if(num == temp) {
+
+		if (num == temp) {
 			System.out.println("Given value is an Armstrong number");
-		}
-		else {
+		} else {
 			System.out.println("Given value is not an Armstrong Number");
 		}
 		scanner.close();
@@ -36,22 +35,22 @@ public class ArmstrongNumber {
 		int temp = num;
 		int rem;
 		int add = 0;
-		while(num > 0) {
+		while (num > 0) {
 			rem = num % 10;
 			add = add + (rem * rem * rem);
 			num = num / 10;
 		}
-		if(temp == add) {
+		if (temp == add) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
+
 	public static int armstrongNumber(int num) {
 		int rem;
 		int add = 0;
-		while(num > 0) {
+		while (num > 0) {
 			rem = num % 10;
 			add = add + (rem * rem * rem);
 			num = num / 10;
