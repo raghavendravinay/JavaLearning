5795d0a0114eab3f770d57f9ca778fa1890662c8
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-08-14 14:17:29 +0530

    modified the code

--- src/strings/ReverseAString.java
+++ src/strings/ReverseAString.java
@@ -1,26 +1,29 @@
 package strings;
 
 import java.util.Scanner;
+
 /**
  * This program is to print the reverse of a string
+ * 
  * @author RaghavendraVinayM
  *
  */
 public class ReverseAString {
 
 	public static void main(String[] args) {
-		
-		String original ;
+
+		String original;
 		String rev = "";
 		System.out.println("Enter string to reverse");
 		Scanner scanner = new Scanner(System.in);
 		original = scanner.nextLine();
 		int len = original.length();
-		for(int i = len - 1; i >= 0; i--) {
-			rev=rev+original.charAt(i);
+		for (int i = len - 1; i >= 0; i--) {
+			rev = rev + original.charAt(i);
 		}
 		System.out.println("Original String is: " + original);
 		System.out.println("reverse of string is: " + rev);
+
 		scanner.close();
 	}
 }
