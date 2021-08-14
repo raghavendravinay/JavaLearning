a0cd2c3a4bb1f2649e12b1b413736e3a398c6d03
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-08-14 14:16:41 +0530

    Modified the code

--- src/oops2/Travels.java
+++ src/oops2/Travels.java
@@ -11,8 +11,76 @@ public class Travels {
 	private String address;
 	private int price;
 
+	public Travels() {
+
+	}
+
+	public String getName() {
+		return passengerName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public int getTickets() {
+		return noofTickets;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public void setTickets(int noofTickets) {
+		this.noofTickets = noofTickets;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void getPrice(int price) {
+		this.price = price;
+	}
+
 	public Travels(String passengerName, int age, String gender, int noofTickets, String from, String to,
-			String address, int price) {
+			String address) {
 		this.passengerName = passengerName;
 		this.age = age;
 		this.gender = gender;
@@ -20,12 +88,11 @@ public class Travels {
 		this.from = from;
 		this.to = to;
 		this.address = address;
-		this.price = price;
 	}
 
 	public String toString() {
 		return "Passenger Name: " + passengerName + ", Age: " + age + ", gender: " + gender + ", No.of tickets: "
-				+ noofTickets + ", From Adress: " + from + ", To Address: " + to + ", TicketPrice: " + price
+				+ noofTickets + ", From Adress: " + from + ", To Address: " + to + ", TicketPrice: " + noofTickets*500
 				+ ", Address: " + address;
 
 	}
