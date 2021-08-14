10707b43491794db5b4f0766f2123a1276f05882
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-08-14 13:49:18 +0530

    Added bulider class for Job Registration class

---
+++ src/oops2/JobRegistrationBuilder.java
@@ -0,0 +1,52 @@
+package oops2;
+
+import java.util.ArrayList;
+
+public class JobRegistrationBuilder {
+
+	ArrayList<JobRegistration> list = new ArrayList<>();
+	
+	public void addApplicantDetails(JobRegistration jr) {
+		boolean flag = false;
+		for(JobRegistration jrg : list) {
+			if(jrg.getName().equals(jr.getName())) {
+				flag = true;
+				break;
+			}
+		}
+		if(!flag) {
+			list.add(jr);
+		}
+	}
+	
+	public boolean removeApplicant(String name) {
+		JobRegistration jrn = null;
+		boolean flag = false;
+		for(JobRegistration jr : list) {
+			if(jr.getName().equals(name)) {
+				jrn = jr;
+				flag = true;
+				break;
+			}
+		}
+		if(flag) {
+			list.remove(jrn);
+			return flag;
+		}
+		return flag;
+	}
+	public void printApplicants() {
+		for(JobRegistration jr : list) {
+			System.out.println(jr);
+		}
+		System.out.println();
+	}
+	public JobRegistration getApplicantDetails(String name) {
+		for(JobRegistration jr : list) {
+			if(jr.getName().equals(name)) {
+				return jr;
+			}
+		}
+		return null;
+	}
+}
