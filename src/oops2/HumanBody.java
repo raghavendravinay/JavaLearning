603f8e82ed4d6d581f1c34088f1e2cf08d9ceb8a
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-08-14 14:13:16 +0530

    Modified the code

--- src/oops2/HumanBody.java
+++ src/oops2/HumanBody.java
@@ -8,24 +8,93 @@ public class HumanBody {
 	private int mouth;
 	private int legs;
 	private int hands;
-	private int body;
 	private int eyes;
+	private String name;
 
-	public HumanBody(int body, int head, int eyes, int nose, int mouth, int ears, int hands, int legs) {
+	public HumanBody() {
 
+	}
+
+	public String getName() {
+		return name;
+	}
+	public int getHeadCount() {
+		return head;
+	}
+
+	public int getNoseCount() {
+		return nose;
+	}
+
+	public int getEarsCount() {
+		return ears;
+	}
+
+	public int getMouthCount() {
+		return mouth;
+	}
+
+	public int getLegscount() {
+		return legs;
+	}
+
+	public int getHandscount() {
+		return hands;
+	}
+
+	public int getEyescount() {
+		return eyes;
+	}
+
+	public int getLegsCount() {
+		return legs;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setHeadCount(int head) {
 		this.head = head;
-		this.body = body;
+	}
+
+	public void setNoseCount(int nose) {
+		this.nose = nose;
+	}
+
+	public void setEyesCount(int eyes) {
+		this.eyes = eyes;
+	}
+
+	public void setMouthCount(int mouth) {
+		this.mouth = mouth;
+	}
+
+	public void setEarsCount(int ears) {
+		this.ears = ears;
+	}
+
+	public void setHandsCount(int hands) {
+		this.hands = hands;
+	}
+
+	public void setLegCount(int legs) {
+		this.legs = legs;
+	}
+
+	public HumanBody(String name, int head, int eyes, int nose, int mouth, int ears, int hands, int legs) {
+
+		this.name = name;
+		this.head = head;
 		this.nose = nose;
 		this.ears = ears;
 		this.mouth = mouth;
 		this.legs = legs;
 		this.hands = hands;
-		this.body = body;
 		this.eyes = eyes;
 	}
 
 	public String toString() {
-		return "Body: " + body + ", Head: " + head + ", Eyes: " + eyes + ", Nose: " + nose + ", Mouth: " + mouth
+		return "Baby name: " + name + ", Head: " + head + ", Eyes: " + eyes + ", Nose: " + nose + ", Mouth: " + mouth
 				+ ", Ears: " + ears + ", Hands: " + hands + ", Legs: " + legs;
 	}
 }
