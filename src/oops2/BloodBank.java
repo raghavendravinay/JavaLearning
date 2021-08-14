b37ce2907d47072a3108918b3c57739cd835950a
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-08-14 14:03:09 +0530

    Changed the emergencynumbers class name to blood bank and modified the code

--- src/oops2/BloodBank.java
+++ src/oops2/BloodBank.java
@@ -1,17 +1,47 @@
 package oops2;
 
-public class EmergencyNumbers {
+public class BloodBank {
 
-	private String Department;
-	private int number;
+	private String name;
+	private String bloodGroup;
+	private long number;
+	private String address;
 
-	public EmergencyNumbers(String Department, int number) {
-		this.Department = Department;
+	public BloodBank(String name, String bloodGroup, long number, String address) {
+		this.name = name;
+		this.bloodGroup = bloodGroup;
+		this.number = number;
+		this.address = address;
+	}
+	public BloodBank() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public String getbloodGroup() {
+		return bloodGroup;
+	}
+	public long getNumber() {
+		return number;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setDepartment(String name) {
+		this.name = name;
+	}
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+	public void setNumber(long number) {
 		this.number = number;
 	}
-
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	public String toString() {
-		return Department + ": " + number;
+		return "Name: " + name + "\nBlood Group" + bloodGroup + "\nNumber:" + number + "\nAddress: " + address + "." ;
 
 	}
 }
