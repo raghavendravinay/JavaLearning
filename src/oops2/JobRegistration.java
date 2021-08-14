dcda9f0f448d5d1c96e197b77a412b6462e2beec
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-08-14 14:13:49 +0530

    Modified the code

--- src/oops2/JobRegistration.java
+++ src/oops2/JobRegistration.java
@@ -8,6 +8,59 @@ public class JobRegistration {
 	private int exp;
 	private String languages;
 	private String address;
+	
+	public JobRegistration() {
+		
+	}
+
+	public String getJobRole() {
+		return jobRole;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getQualification() {
+		return qualification;
+	}
+	
+	public int getExperience() {
+		return exp;
+	}
+	
+	public String getLanguages() {
+		return languages;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setJobRole(String jobRole) {
+		this.jobRole = jobRole;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+	public void setExperience(int exp) {
+		this.exp = exp;
+	}
+
+	public void setLanguages(String languages) {
+		this.languages = languages;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 
 	public JobRegistration(String name, String jobRole, String qualification, int exp, String languages, String address) {
 		this.name = name;
