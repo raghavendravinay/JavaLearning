50e14c62e810e6a0cbade050fd67ba1e43f1a262
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-08-14 14:10:05 +0530

    Modified the code

--- src/oops2/Employee.java
+++ src/oops2/Employee.java
@@ -2,20 +2,56 @@ package oops2;
 
 public class Employee {
 
-	private String EmployeeName;
+	private String employeeName;
 	private int exp;
 	private int id;
 	private String designation;
 
-	public Employee(String EmployeeName, int exp, int id, String designation) {
-		this.EmployeeName = EmployeeName;
+	public Employee() {
+
+	}
+
+	public Employee(String employeeName, int exp, int id, String designation) {
+		this.employeeName = employeeName;
 		this.exp = exp;
 		this.id = id;
 		this.designation = designation;
 	}
 
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public int getExperience() {
+		return exp;
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public void setExperience(int exp) {
+		this.exp = exp;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
 	public String toString() {
-		return "Employee Name: " + EmployeeName + ", Experience: " + exp + ", ID: " + id + ", Designation: "
+		return "Employee Name: " + employeeName + ", Experience: " + exp + ", ID: " + id + ", Designation: "
 				+ designation;
 	}
 }
