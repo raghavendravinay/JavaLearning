c0c7717290b545c9904128f1a7313c7bd3eaaa1a
VinayR <59249569+RaghavendraVinayM@users.noreply.github.com>
2021-08-14 14:07:16 +0530

    Modified the code

--- src/oops2/CoachingClass.java
+++ src/oops2/CoachingClass.java
@@ -4,17 +4,22 @@ public class CoachingClass {
 
 	public static void main(String[] args) {
 
+		CoachingCenterBuilder ccb = new CoachingCenterBuilder();
 		CoachingCenter cc = new CoachingCenter("Vinay", "Java", "Tanuku");
-		System.out.println(cc);
-		CoachingCenter cc1 = new CoachingCenter();
-		cc1.setName("Raghav");
-		cc1.setCourse(".net");
-		cc1.setAddress("address");
-		System.out.println(cc1);
-		CoachingCenter cc2 = new CoachingCenter();
-		cc2.setName("M.R. Vinay");
-		cc2.setCourse("Python");
-		cc2.setAddress("Hyderabad");
-		System.out.println("Name: " + cc2.getName() + ", course: " + cc2.getCourse() + ", Address: " + cc2.getAddress());
+		ccb.registerStudent(cc);
+		CoachingCenter cc1 = new CoachingCenter("Ravi", "Python", "Hyderabad");
+		ccb.registerStudent(cc1);
+		CoachingCenter cc2 = new CoachingCenter("Raghav", "c#", "Tanuku");
+		ccb.registerStudent(cc2);
+		ccb.printStudents();
+		CoachingCenter cc3 = new CoachingCenter("Raghav", "c#", "Tanuku");
+		ccb.registerStudent(cc3);
+		ccb.printStudents();
+		System.out.println(ccb.removeStudent("Raghav"));
+		ccb.printStudents();
+		System.out.println(ccb.removeStudent("Raghav"));
+		ccb.printStudents();
+		System.out.println(ccb.getStudents("Ravi"));
+		System.out.println(ccb.getStudents("Raghav"));
 	}
 }
